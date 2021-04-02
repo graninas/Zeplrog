c8eb24c60b68a683802d432ae3e8083dadb32c36
Alexander Granin <graninas@gmail.com>
2021-04-02 20:56:08 +0700

    Small improvement

--- src/ZP/Game/Logic.hs
+++ src/ZP/Game/Logic.hs
@@ -8,14 +8,16 @@ import ZP.Game.Types
 
 import qualified Data.Map as Map
 
+animateActorPath :: ActorState -> STM ()
+animateActorPath ActorState {..} = do
+  dispVar <- readTVar currentPathDisplayVar
+  case dispVar of
+    PathIsInvisible           -> pure ()
+    PathIsBlinking n | n > 0  -> writeTVar currentPathDisplayVar $ PathIsBlinking $ n - 1   -- TODO: not hardcoded blink period
+    PathIsBlinking n | n == 0 -> writeTVar currentPathDisplayVar $ PathIsBlinking $ 3       -- TODO: not hardcoded blink period
+
 
 simpleGameSimulator :: Float -> GameState -> IO GameState
 simpleGameSimulator _ st@(GameState {..}) = do
-  let ActorState {..} = playerActorState
-  atomically $ do
-    dispVar <- readTVar currentPathDisplayVar
-    case dispVar of
-      PathIsInvisible           -> pure ()
-      PathIsBlinking n | n > 0  -> writeTVar currentPathDisplayVar $ PathIsBlinking $ n - 1   -- TODO: not hardcoded blink period
-      PathIsBlinking n | n == 0 -> writeTVar currentPathDisplayVar $ PathIsBlinking $ 3       -- TODO: not hardcoded blink period
+  atomically $ animateActorPath playerActorState
   pure st
