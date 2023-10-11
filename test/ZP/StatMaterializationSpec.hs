7b6a945f19e817ccb4bb50fa8eac830da658ec41
Alexander Granin <graninas@gmail.com>
2023-10-11 17:37:42 +0300

    Tests fixed.

--- test/ZP/StatMaterializationSpec.hs
+++ test/ZP/StatMaterializationSpec.hs
@@ -6,7 +6,7 @@ import ZP.Prelude
 
 import ZP.System.Debug
 import ZP.Domain.Static.Model
-import ZP.Domain.Static.Materialization
+import ZP.Domain.Materializer
 import qualified ZP.Assets.KnowledgeBase as KB
 
 import Test.Hspec
@@ -22,17 +22,18 @@ spec = do
     it "Door materialization test" $ do
       sEnv@(SEnv _ statPropsVar) <- makeSEnv DebugDisabled
 
-      (ess, door) <- sMat' sEnv $ Proxy @KB.Door
+      (Ess ess, door) <- sMat' sEnv $ Proxy @KB.Door
 
       statProps <- readTVarIO statPropsVar
       length statProps `shouldBe` 7
 
       case door of
-        PropDict root props -> do
-          length props `shouldBe` 5
+        PropDict root props -> length props `shouldBe` 5
         _ -> error "invalid materialization result"
 
-    xit "Game materialization test" $ do
+      ess `shouldBe` "object:door"
+
+    it "Game materialization test" $ do
       sEnv@(SEnv _ statPropsVar) <- makeSEnv DebugDisabled
 
       game <- sMat' sEnv $ Proxy @KB.Zeplrog
@@ -43,5 +44,5 @@ spec = do
 
       let GameEnvironment props triggs = game
 
-      length props `shouldBe` 543
-      length triggs `shouldBe` 66
+      length props `shouldBe` 1
+      length triggs `shouldBe` 4
